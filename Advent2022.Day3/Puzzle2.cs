20865e5cc55321298ba8895e0e13d7a149504902
C1rdec <cedric_lampron@hotmail.com>
2022-12-03 17:49:56 -0500

    Fix namespace

--- Advent2022.Day3/Puzzle2.cs
+++ Advent2022.Day3/Puzzle2.cs
@@ -1,28 +1,27 @@
-﻿using Advent2022.Core;
+﻿namespace Advent2022.Day3;
+
+using Advent2022.Core;
 using Advent2022.Day3.Models;
 
-namespace Advent2022.Day3
+public class Puzzle2 : PuzzleBase
 {
-    public class Puzzle2 : PuzzleBase
+    protected override string Solve(string input)
     {
-        protected override string Solve(string input)
+        var lines = input.Split(Environment.NewLine);
+
+        var sum = 0;
+        var skip = 0;
+        do
         {
-            var lines = input.Split(Environment.NewLine);
+            var sacks = lines.Skip(skip).Take(3).Select(l => new Rubsack(l));
+            var group = new Group(sacks);
 
-            var sum = 0;
-            var skip = 0;
-            do
-            {
-                var sacks = lines.Skip(skip).Take(3).Select(l => new Rubsack(l));
-                var group = new Group(sacks);
+            sum += group.FindBadge();
 
-                sum += group.FindBadge();
-
-                skip += 3;
-            }
-            while (lines.Length > skip);
-
-            return sum.ToString();
+            skip += 3;
         }
+        while (lines.Length > skip);
+
+        return sum.ToString();
     }
 }
